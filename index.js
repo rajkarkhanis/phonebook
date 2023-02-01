e094605d52823f2a74c73d2540fc7bf76d7ae74c
rajkarkhanis <rkarkhanis00@gmail.com>
2023-02-01 15:12:01 +0530

    error handling

--- index.js
+++ index.js
@@ -27,49 +27,70 @@ let phonebook = [
 ];
 
 const generateId = () => {
-    return Math.floor(Math.random() * (500 - 5) + 5)
-}
+	return Math.floor(Math.random() * (500 - 5) + 5);
+};
 
 // Return phonebook
 app.get("/api/persons", (request, response) => {
-    response.json(phonebook)
-})
+	response.json(phonebook);
+});
 
 // Return info of a single person
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = phonebook.find(person => person.id === id)
-    
-    person ? response.json(person) : response.status(404).end()
-})
+	const id = Number(request.params.id);
+	const person = phonebook.find((person) => person.id === id);
 
+	person ? response.json(person) : response.status(404).end();
+});
 
 // Add a person to phonebook
 app.post("/api/persons", (request, response) => {
-    const person = {
-        id: generateId(),
-        name: request.body.name,
-        number: request.body.number
-    }
+	const body = request.body;
 
-    phonebook = phonebook.concat(person)
-    response.json(person)
-})
+	if (!body.name) {
+		return response.status(400).json({
+			error: "Name is missing",
+		});
+	}
+
+	if (!body.number) {
+		return response.status(400).json({
+			error: "Number is missing",
+		});
+	}
+
+	const exists = phonebook.some((person) => person.name.includes(body.name));
+
+	if (exists) {
+		return response.status(400).json({
+			error: "Name already exists in phonebook",
+		});
+	}
+
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number,
+	};
+
+	phonebook = phonebook.concat(person);
+	response.json(person);
+});
 
 // Delete a person from phonebook
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    phonebook = phonebook.filter(person => person.id === id)
-    response.status(204).end()
-})
+	const id = Number(request.params.id);
+	phonebook = phonebook.filter((person) => person.id === id);
+	response.status(204).end();
+});
 
 // Return info about phonebook
 app.get("/api/persons/info", (request, response) => {
-    response.send(`
+	response.send(`
         <p>Phonebook has ${phonebook.length} entries</p>
         <p>${new Date()}</p>
-    `)
-})
+    `);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
